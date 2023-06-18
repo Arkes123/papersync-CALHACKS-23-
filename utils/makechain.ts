9d2e9659679c48102c21a510e2135d23b211ddb2
Jerry <jeli04.2020@gmail.com>
2023-06-18 11:11:53 -0700

    wokring on langchain problems

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,15 +1,18 @@
 import { OpenAI, PromptLayerOpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { LLMChain, loadQAChain, ChatVectorDBQAChain, ConversationChain } from 'langchain/chains';
+import { LLMChain, loadQAChain, VectorDBQAChain, ConversationalRetrievalQAChain, RetrievalQAChain } from 'langchain/chains';
 import { PromptTemplate } from 'langchain/prompts';
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BufferMemory } from "langchain/memory";
+import { resourceLimits } from 'worker_threads';
 
-const prompt =
-    PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const broad_prompt =
+    `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
     Chat History:
     {chat_history}
     Follow Up Input: {question}
-    Standalone question:`);
+    Standalone question:`;
 
 const detailed_prompt = 
 
@@ -25,24 +28,39 @@ Question: {question}
 Helpful answer in markdown:`);
 
     export const makeChain = (vectorstore: PineconeStore) => {
-        const questionGenerator = new LLMChain({
-          llm: new OpenAI({ temperature: 0 }),
-          prompt: prompt,
-        });
-      
-        const docChain = loadQAChain(
-          //change modelName to gpt-4 if you have access to it
-          new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' }),
-          {
-            prompt: detailed_prompt,
-          },
-        );
-      
-        return new ConversationChain({
-          vectorstore,
-          combineDocumentsChain: docChain,
-          questionGeneratorChain: questionGenerator,
+      const questionGenerator = new LLMChain({
+        llm: new OpenAI({ temperature: 0 }),
+        prompt: broad_prompt,
+      });
+    
+      const docChain = loadQAChain(
+        //change modelName to gpt-4 if you have access to it
+        new OpenAI({ temperature: 0, modelName: 'gpt-4-0613' }),
+        {
+          prompt: detailed_prompt,
+        },
+      );
+
+      const model = new ChatOpenAI({
+        modelName: "gpt-4-0613",
+      });
+
+    return ConversationalRetrievalQAChain.fromLLM(
+        model,
+        vectorstore.asRetriever(),
+        {
           returnSourceDocuments: true,
-          k: 1, //number of source documents to return. Change this figure as required.
-        });
-      };
+          memory: new BufferMemory({
+            memoryKey: "chat_history",
+            inputKey: "question", // The key for the input to the chain
+            outputKey: "text", // The key for the final conversational output of the chain
+            returnMessages: true, // If using with a chat model
+          }),
+          questionGeneratorChainOptions: {
+            llm: model,
+            template: broad_prompt
+          },
+        
+        }
+    );
+    };
